7c0a17a5d133e2020b1a6f61393c5115b7988d13
LH473 <lapo.hirschhofer@gmail.com>
2025-06-09 18:28:51 +0200

    Update Trader.py

--- Trader.py
+++ Trader.py
@@ -6,6 +6,8 @@ from backtesting import Backtest
 from backtesting.test import GOOG
 from backtesting.lib import crossover
 
+
+input1: 'SPY'
 dataF=yf.download('SPY', period='5y')
 if isinstance(dataF.columns, pd.MultiIndex):
         dataF.columns = dataF.columns.get_level_values(0)
@@ -19,20 +21,41 @@ def closing(data):
 def EWM(data, n):
      return pd.Series(data).ewm(span=n, adjust=True).mean()
      
+def RSI(dataF, days):
+    positive=delta.copy()
+    negative=delta.copy()
+
+    positive[positive < 0] = 0
+    negative[negative > 0] = 0
+
+    days = 14
+
+    average_gain = positive.rolling(window=days).mean()
+    average_loss = abs(negative.rolling(window=days).mean())
+    relative_strength = average_gain/average_loss
+
+    RSI_var = 100 - (100 / (1 + relative_strength))
+
+    combined = pd.DataFrame()
+    combined['RSI'] = RSI_var
+
+    return pd.Series(RSI_var)
 
 class golden_setup(Strategy):
+    RSI_span = 14
     short_EWM_span = 50
     long_EWM_span = 200
 
 
     def init(self):
+        self.RSI = self.I(RSI, self.data, self.RSI_span)
         self.short_EWM = self.I(EWM, self.data.Close, self.short_EWM_span)
         self.long_EWM = self.I(EWM, self.data.Close, self.long_EWM_span)
 
     def next(self):
         #Actual strategy if x: buy, elif y: sell
         try:
-            if self.short_EWM[-1] > self.long_EWM[-1] or crossover(self.short_EWM[-1], self.longEWM[-1]):
+            if self.short_EWM[-1] > self.long_EWM[-1] or crossover(self.short_EWM[-1], self.longEWM[-1]) or crossover(self.RSI[-1], 30):
                 self.position.close()
                 self.buy()
             elif self.short_EWM[-1] < self.long_EWM[-1]:
