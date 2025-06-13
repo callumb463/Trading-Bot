b30074e969ef5d7f57fd56f957e5d252ec9a7962
callumb463 <callumbrown101@gmail.com>
2025-06-12 22:06:11 -0700

    Update Trader.py

--- Trader.py
+++ Trader.py
@@ -32,14 +32,16 @@ def RSI(data, days):
     RSI_var = 100 - (100 / (1 + relative_strength))
 
     return pd.Series(RSI_var)
-     
 
 
 
 class golden_setup(Strategy):
     RSI_span = 14
+    RSI_upper = 70
+    RSI_lower = 30
     short_EWM_span = 50
     long_EWM_span = 200
+    n = 5
 
 
     def init(self):
@@ -48,15 +50,21 @@ class golden_setup(Strategy):
         self.long_EWM = self.I(EWM, self.data.Close, self.long_EWM_span)
 
     def next(self):
-        #EACH POSITION IS AN INSTANCE OF CLASS. JUST CREATE NEW ONES
-        EWM_condition = crossover(self.short_EWM[-1], self.long_EWM[-1]) and np.diff(self.short_EWM, prepend=0)[-1] > np.diff(self.long_EWM, prepend=0)[-1]
-        if  self.short_EWM[-1] > self.long_EWM[-1] or EWM_condition or crossover(self.RSI[-1], 30):
-                self.buy(size=2)
+        EWM_condition =  self.short_EWM[-1] > self.long_EWM[-1] and np.diff(self.short_EWM, prepend=0)[-1] > np.diff(self.long_EWM, prepend=0)[-1]
+        RSI_buy = self.RSI[-1] < self.RSI_lower and np.mean(np.diff(self.data.Close,prepend=0)[-self.n:-1]) < 0 and np.diff(self.RSI, prepend=0)[-1] > 0
+        RSI_sell = self.RSI[-1] > self.RSI_upper and np.mean(np.diff(self.data.Close,prepend=0)[-self.n:-1]) > 0 and np.diff(self.RSI, prepend=0)[-1] < 0
+        RSI_weight = np.dot([np.diff(self.RSI, prepend=0)[-1], 1]/np.linalg.norm([np.diff(self.RSI, prepend=0)[-1], 1]),[np.mean(np.diff(self.data.Close,prepend=0)[-self.n:-1]),1]/np.linalg.norm([np.mean(np.diff(self.data.Close,prepend=0)[-self.n:-1]),1]))
+
+        condition = 0.5*EWM_condition +0.5*(crossover(self.short_EWM[-1], self.long_EWM[-1]))+ RSI_weight*RSI_buy - 1*RSI_weight*RSI_sell - 1*(self.short_EWM[-1] < self.long_EWM[-1])
+        print(condition)
+        #RSI IS NOW PROPERLY IMPLEMENTED BUT BECAUSE WE STILL BUY EVERYTHING WAY TO QUICKLY IT'S EFFECTS CANNOT BE SEEN
+        if condition>=0.5:
+                self.buy(size=1)
                 
-        elif self.short_EWM[-1] < self.long_EWM[-1]:# or crossover(70, self.RSI[-1]):
+        elif condition<0:
                 self.position.close()
 
-bt = Backtest(dataF, golden_setup, cash=10_000, commission=0.0,exclusive_orders=False)
+bt = Backtest(dataF, golden_setup, cash=10_000, commission=0.0,exclusive_orders=False,finalize_trades=True)
 stats = bt.run()
 print(stats)
 bt.plot()
