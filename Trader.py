f1887b891e5807badd6965459b309f986419eb3e
callumb463 <callumbrown101@gmail.com>
2025-06-13 15:33:14 -0700

    Update Trader.py

--- Trader.py
+++ Trader.py
@@ -2,7 +2,6 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import sambo
-import matplotlib.pyplot as plt
 from sambo.plot import plot_objective
 from backtesting import Strategy
 from backtesting import Backtest
